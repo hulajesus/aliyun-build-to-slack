dd4a0edb4a9845ba0fdeca1cc34ce6881725f69b
Kai <kai@thekaiway.com>
2018-01-10 15:57:05 +0800

    Fix command not found error of script

--- send-to-slack.sh
+++ send-to-slack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-msg=$(echo "payload={\"channel\": \"#dev\", \"text\": \"`$1:$2` built at $3\"}")
+msg=$(echo "payload={\"channel\": \"#dev\", \"text\": \"\`$1:$2\` built at $3\"}")
 curl -X POST --data-urlencode "$msg" $SLACK_URL
